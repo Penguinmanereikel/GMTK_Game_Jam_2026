7b3e677ca5c485529abfbd7b7abb29b15c384ce4
--penguinmanereikel <--gafurulkazi98@gmail.com>
2026-07-24 23:15:39 -0400

    Completed conversation system

--- scripts/dialogue_box.gd
+++ scripts/dialogue_box.gd
@@ -12,7 +12,7 @@ const CONVERSATION_DICT = {
 		["Player", "Hey, do you have the champagne delivery?"],
 		["MailGuy", "Uh, yeah. I think it's here somewhere..."],
 		["Player", "Great, can you give it to me to give it to the bosses?"],
-		["MailGuy", "Uh, sure, but, I gotta sort through all this junk mail, and fanmail, and federal notices, and"],
+		["MailGuy", "Uh, sure, but, I gotta sort through all this junk mail, and fanmail, and federal notices, and..."],
 		["Player", "ARGH! JUST LET ME DO IT!"]
 	]
 }
@@ -33,6 +33,9 @@ var convo_index: int:
 		if convo_index < len(active_convo):
 			show_dialogue(active_convo[value][1], CONVERSATION_COLORS[active_convo[value][0]])
 
+const OPEN_POSITION_Y: float = 539.0
+const CLOSE_POSITION_Y: float = 789.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	active = false
@@ -50,19 +53,26 @@ func start_convo(id):
 	open()
 	
 func show_dialogue(text, color):
-	#$Sprite2D.call('modulate', color)
+	$Sprite2D.set_modulate(color)
 	$Control/RichTextLabel.text = text
 	animate_dialogue()
 
 func animate_dialogue():
 	var text_tween = create_tween()
 	$Control/RichTextLabel.visible_ratio = 0.0
+	active = false
 	text_tween.tween_property($Control/RichTextLabel, "visible_ratio", 1.0, 1.0)
+	text_tween.finished.connect(func (): active = true)
 
 func open():
-	active = true
 	print("Open")
+	#active = true
+	var box_tween = create_tween()
+	box_tween.tween_property($".", "position:y", OPEN_POSITION_Y, 0.25)
+	
 	
 func close():
-	active = false
 	print("Close")
+	#active = false
+	var box_tween = create_tween()
+	box_tween.tween_property($".", "position:y", CLOSE_POSITION_Y, 0.25)
