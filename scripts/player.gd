a228f8ee5d7e6aaade634eae2637b26b36af3a1f
--penguinmanereikel <--gafurulkazi98@gmail.com>
2026-07-25 12:24:25 -0400

    Adjusted tilemap and level map and camera

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var playerSpeed = 20
+var dir: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,7 +10,24 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	dir = 0
 	if Input.is_action_pressed("Left"):
-		self.position.x -= playerSpeed * delta
+		dir -= 1
 	if Input.is_action_pressed("Right"):
-		self.position.x += playerSpeed * delta
+		dir += 1
+	
+	print(dir)
+	print((playerSpeed * delta) * dir)
+	self.position.x += (playerSpeed * delta) * dir
+	
+	if dir == 0:
+		$AnimatedSprite2D.play('Idle')
+	elif dir < 0:
+		if scale.x > 0:
+			scale.x *= -1
+		$AnimatedSprite2D.play("Walk")
+	elif dir > 0:
+		if scale.x < 0:
+			scale.x *= -1
+		$AnimatedSprite2D.play("Walk")
+	
