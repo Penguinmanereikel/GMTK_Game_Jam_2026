5a05b6ed7a80c9e804604a252395d4b5868b0ab2
--penguinmanereikel <--gafurulkazi98@gmail.com>
2026-07-26 12:18:05 -0400

    fixed movement, adjusted level map

--- scripts/minigame_storage.gd
+++ scripts/minigame_storage.gd
@@ -10,8 +10,7 @@ var clicked: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Timer.start()
-	
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- scripts/player.gd
+++ scripts/player.gd
@@ -11,12 +11,19 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	dir = 0
+	print($AnimatedSprite2D.scale.x)
 	#print("Player status: ", Globals.dialogue_open, " ", Globals.minigame_open)
 	if Globals.dialogue_open == false and Globals.minigame_open == false and Globals.overscreen_present == false:
 		if Input.is_action_pressed("Left"):
 			dir -= 1
 		if Input.is_action_pressed("Right"):
 			dir += 1
+		if Input.is_action_just_pressed("Left") and $AnimatedSprite2D.scale.x > 0:
+			print("HIT LEFT")
+			$AnimatedSprite2D.scale.x *= -1
+		if Input.is_action_just_pressed("Right") and $AnimatedSprite2D.scale.x < 0:
+			print("HIT RIGHT")
+			$AnimatedSprite2D.scale.x *= -1
 		
 		#print((playerSpeed * delta) * dir)
 		velocity.x = playerSpeed * dir
@@ -26,9 +33,7 @@ func _process(delta):
 		if dir == 0:
 			$AnimatedSprite2D.play('Idle')
 		elif dir < 0:
-			scale.x = -1
 			$AnimatedSprite2D.play("Walk")
 		elif dir > 0:
-			scale.x = 1
 			$AnimatedSprite2D.play("Walk")
 	
